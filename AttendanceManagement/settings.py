8ba5ad143ffc2f9bd6b5851247495e88d6ded611
tarun <tarunsree@gmail.com>
2025-07-08 15:34:53 +0530

    feat: Update project configuration and settings; add template directory and adjust requirements

--- AttendanceManagement/settings.py
+++ AttendanceManagement/settings.py
@@ -41,6 +41,23 @@ INSTALLED_APPS = [
     'axes',
 ]
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'accounts.context_processors.custom_context_processor',
+            ],
+        },
+    }
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
