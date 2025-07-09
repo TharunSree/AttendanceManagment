37b528597798bde4be66e7f394ac1295f2f8214b
Tharun Sreejith <73305454+TharunSree@users.noreply.github.com>
2025-07-10 04:36:59 +0530

    Update views.py

--- academics/views.py
+++ academics/views.py
@@ -27,10 +27,7 @@ from django.utils import timezone
 from openpyxl import Workbook
 from openpyxl.styles import Font, Alignment
 from pyppeteer import launch
-import subprocess
-import logging
 
-logger = logging.getLogger(__name__)
 
 from academics.forms import EditStudentForm, AttendanceSettingsForm, TimeSlotForm, MarkAttendanceForm, \
     TimetableEntryForm, SubstitutionForm, AttendanceReportForm, AnnouncementForm, CriterionFormSet, MarkingSchemeForm, \
@@ -44,7 +41,10 @@ from .models import StudentGroup, AttendanceSettings, Course, Subject, \
     Timetable, AttendanceRecord, CourseSubject, TimeSlot, ClassCancellation, DailySubstitution, Announcement, \
     UserNotificationStatus, MarkingScheme, Mark, Criterion, ExtraClass, AcademicSession, ResultPublication, \
     StudentSubjectStatus
+import subprocess
+import logging
 
+logger = logging.getLogger(__name__)
 
 # ... other views ...
 
